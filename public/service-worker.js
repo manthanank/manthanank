7da7f6a1f1758ef1f0fd8e98acc5966d83dde240
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-04-25 14:15:28 +0530

    feat: Add initial project files including .eslintrc.json, .gitignore, service worker, and manifest

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'manthanank-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
